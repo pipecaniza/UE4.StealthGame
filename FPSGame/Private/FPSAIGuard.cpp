ebb519b476dac3b025b6188de71db13e490a7672
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-05-26 19:27:10 -0500

    Added AI character

---
+++ FPSGame/Private/FPSAIGuard.cpp
@@ -0,0 +1,169 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSAIGuard.h"
+#include "Perception/PawnSensingComponent.h"
+#include "DrawDebugHelpers.h"
+#include "TimerManager.h"
+#include "FPSGameMode.h"
+#include "AI/Navigation/NavigationSystem.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+AFPSAIGuard::AFPSAIGuard()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
+
+	PawnSensingComp->OnSeePawn.AddDynamic(this, &AFPSAIGuard::OnPawnSeen);
+	PawnSensingComp->OnHearNoise.AddDynamic(this, &AFPSAIGuard::OnNoiseHeard);
+
+	FirstPatrolPoint = CreateDefaultSubobject<AActor>(TEXT("FirstPatrolPoint"));
+	SecondPatrolPoint = CreateDefaultSubobject<AActor>(TEXT("SecondPatrolPoint"));
+	
+	CurrentPatrolPoint = nullptr;
+
+	GuardState = EAIState::Idle;
+}
+
+// Called when the game starts or when spawned
+void AFPSAIGuard::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OriginalRotation = GetActorRotation();
+
+	if (bPatrol)
+	{
+		MoveToNextPatrolPoint();
+	}
+}
+
+void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
+{
+	if (SeenPawn == nullptr)
+	{
+		return;
+	}
+
+	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Red, false, 10.0f);
+
+	AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+	if (GM)
+	{
+		GM->CompleteMission(SeenPawn, false);
+	}
+
+	SetGuardState(EAIState::Alert);
+
+	AController* Controller = GetController();
+	if (Controller)
+	{
+		Controller->StopMovement();
+	}
+}
+
+void AFPSAIGuard::OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume)
+{
+	if (GuardState == EAIState::Alert)
+	{
+		return;
+	}
+
+	DrawDebugSphere(GetWorld(), Location, 32.0f, 12, FColor::Green, false, 10.0f);
+
+	FVector Direction = Location - GetActorLocation();
+	Direction.Normalize();
+
+	FRotator NewLookAt = FRotationMatrix::MakeFromX(Direction).Rotator();
+	NewLookAt.Pitch = 0.0f;
+	NewLookAt.Roll = 0.0f;
+
+	SetActorRotation(NewLookAt);
+
+	GetWorldTimerManager().ClearTimer(TimerHandle_ResetOrientation);
+	GetWorldTimerManager().SetTimer(TimerHandle_ResetOrientation, this, &AFPSAIGuard::ResetOrientation, 3.0f, false);
+
+	SetGuardState(EAIState::Suspicius);
+
+	AController* Controller = GetController();
+	if (Controller)
+	{
+		Controller->StopMovement();
+	}
+}
+
+void AFPSAIGuard::ResetOrientation()
+{
+	if (GuardState == EAIState::Alert)
+	{
+		return;
+	}
+
+	SetActorRotation(OriginalRotation);
+	SetGuardState(EAIState::Idle);
+
+	if (bPatrol)
+	{
+		MoveToNextPatrolPoint();
+	}
+}
+
+void AFPSAIGuard::OnRep_GuardState()
+{
+	OnStateChanged(GuardState);
+}
+
+void AFPSAIGuard::SetGuardState(EAIState NewState)
+{
+	if (GuardState == NewState)
+	{
+		return;
+	}
+	GuardState = NewState;
+
+	OnRep_GuardState();	
+}
+
+void AFPSAIGuard::MoveToNextPatrolPoint()
+{
+	if (CurrentPatrolPoint == nullptr || CurrentPatrolPoint == SecondPatrolPoint)
+	{
+		CurrentPatrolPoint = FirstPatrolPoint;
+	}
+	else
+	{
+		CurrentPatrolPoint = SecondPatrolPoint;
+	}
+
+	UNavigationSystem::SimpleMoveToActor(GetController(), CurrentPatrolPoint);
+}
+
+
+// Called every frame
+void AFPSAIGuard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (CurrentPatrolPoint)
+	{
+		FVector Detal = GetActorLocation() - CurrentPatrolPoint->GetActorLocation();
+		float DistanceToGoal = Detal.Size();
+		UE_LOG(LogTemp, Warning, TEXT("%f"),DistanceToGoal);
+		if (DistanceToGoal < 80)
+		{
+			MoveToNextPatrolPoint();
+		}
+	}
+
+}
+
+
+void AFPSAIGuard::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AFPSAIGuard, GuardState);
+}
+
---
+++ FPSGame/Public/FPSAIGuard.h
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FPSAIGuard.generated.h"
+
+UENUM(BlueprintType)
+enum class EAIState : uint8
+{
+	Idle,
+
+	Suspicius,
+
+	Alert
+};
+
+UCLASS()
+class FPSGAME_API AFPSAIGuard : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPSAIGuard();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UPawnSensingComponent* PawnSensingComp;
+
+	UFUNCTION()
+	void OnPawnSeen(APawn* SeenPawn);
+
+	UFUNCTION()
+	void OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume);
+
+	FRotator OriginalRotation;
+
+	UFUNCTION()
+	void ResetOrientation();
+		
+	FTimerHandle TimerHandle_ResetOrientation;
+
+	UPROPERTY(ReplicatedUsing=OnRep_GuardState)
+	EAIState GuardState;
+
+	UFUNCTION()
+	void OnRep_GuardState();
+
+	void SetGuardState(EAIState NewState);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "AI")
+	void OnStateChanged(EAIState NewState);
+
+	UPROPERTY(EditInstanceOnly, Category = "AI")
+	bool bPatrol;
+
+	UPROPERTY(EditAnywhere, Category = "AI", meta = (EditCondition = "bPatrol"))
+	AActor* FirstPatrolPoint;
+
+	UPROPERTY(EditAnywhere, Category = "AI", meta = (EditCondition = "bPatrol"))
+	AActor* SecondPatrolPoint;
+
+	AActor* CurrentPatrolPoint;
+	
+	void MoveToNextPatrolPoint();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;	
+	
+};
