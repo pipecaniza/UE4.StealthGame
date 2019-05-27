131dc5fe30ff22fcbc538986340478affd62919e
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-05-26 19:25:01 -0500

    Added GameMode class

---
+++ FPSGame/Private/FPSGameMode.cpp
@@ -0,0 +1,55 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "FPSGameMode.h"
+#include "FPSHUD.h"
+#include "FPSCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "FPSGameState.h"
+
+AFPSGameMode::AFPSGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/BP_Player"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AFPSHUD::StaticClass();
+
+	GameStateClass = AFPSGameState::StaticClass();
+}
+
+void AFPSGameMode::CompleteMission(APawn* InstigatorPawn, bool bMissionSuccess)
+{
+	if (InstigatorPawn)
+	{
+		if (SpectatingViewpointClass)
+		{
+			TArray<AActor*> ReturnedActors;
+			UGameplayStatics::GetAllActorsOfClass(this, SpectatingViewpointClass, ReturnedActors);
+
+			// Change viewtarget if  any valid actor found
+			if (ReturnedActors.Num() > 0)
+			{
+				AActor* NewViewTarget = ReturnedActors[0];
+
+				for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
+				{
+					APlayerController* PC = It->Get();
+					if (PC)
+					{
+						PC->SetViewTargetWithBlend(NewViewTarget, 0.5f, VTBlend_Cubic);
+					}
+				}				
+			}
+		}
+	}
+
+	AFPSGameState* GS = GetGameState<AFPSGameState>();
+	if (GS)
+	{
+		GS->MulticastOnMissionComplete(InstigatorPawn, bMissionSuccess);
+	}
+	
+	OnMissionCompleted(InstigatorPawn, bMissionSuccess);	
+}
